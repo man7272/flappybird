1a16265e02e016ce9246c0473a420220286896fb
man7272 <93382002+man7272@users.noreply.github.com>
2022-09-16 02:39:35 -0700

    Update flappy_bird.py

--- flappy_bird.py
+++ flappy_bird.py
@@ -7,3 +7,20 @@ import pygame
 import sys
 import random
 import time
+
+# START PYGAME MODULES
+pygame.init()
+
+# ALL VARIABLE
+display_width = 576
+display_height = 1024
+floor_x = 0
+gravity = 0.25
+bird_movement = 0
+pipe_list = []
+game_status = True
+bird_list_index = 0
+game_font = pygame.font.Font('assets/font/Flappy.TTF', 40)
+score = 0
+high_score = 0
+active_score = True
